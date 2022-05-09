af33ef2ea6a6252b447510f8edfeb903f1009a01
Alex <alex@Alexs-MacBook-Pro.local>
2022-05-09 12:19:27 -0700

    WIP steps in context learning

--- polaris-react/src/components/InContextLearning/InContextLearning.tsx
+++ polaris-react/src/components/InContextLearning/InContextLearning.tsx
@@ -1,37 +1,83 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
 import {Step} from './components';
 import {Button} from '../Button';
+import {styles} from '../CustomProperties/styles';
 
-interface Props {
-    children: React.ReactElement[];
+interface InContextLearningStep {
+  className: string;
+  content: React.ReactNode;
 }
 
-export function InContextLearning({children}: Props) {
-    const hasMultipleSteps = children.length > 0;
-    const [currentStep, setCurrentStep] = useState(0);
+interface Position {
+  top: number;
+  left: number;
+}
 
-    const showPrev = hasMultipleSteps && currentStep > 0;
-    const showNext = hasMultipleSteps && currentStep < (children.length - 1);
+interface Props {
+  children?: React.ReactElement[];
+  steps: InContextLearningStep[];
+}
 
-    const handleNext = () => {
-        setCurrentStep((currentStep) => currentStep + 1)
-        console.warn('Next clicked');
+export function InContextLearning({children, steps}: Props) {
+  const hasMultipleSteps = steps.length > 0;
+  const [currentStep, setCurrentStep] = useState(0);
+  const [currentPosition, setCurrentPosition] = useState<Position | null>(null);
+  const wrapperRef = useRef(null);
+
+  useEffect(() => {
+    setCurrentPosition(updatePosition(steps[currentStep]));
+  }, []);
+
+  const showPrev = hasMultipleSteps && currentStep > 0;
+  const showNext = hasMultipleSteps && currentStep < steps.length - 1;
+
+  const handleNext = () => {
+    setCurrentStep((currentStep) => currentStep + 1);
+    console.warn('Next clicked');
+  };
+
+  const handlePrev = () => {
+    setCurrentStep((currentStep) => currentStep - 1);
+    console.warn('Prev clicked');
+  };
+
+  useEffect(() => {
+    if (!wrapperRef.current) {
+      return;
     }
+    const {height} = wrapperRef.current.getBoundingClientRect();
+    setCurrentPosition(updatePosition(steps[currentStep], height));
+  }, [currentStep, steps]);
 
-    const handlePrev = () => {
-        setCurrentStep((currentStep) => currentStep - 1)
-        console.warn('Prev clicked');
-    }
+  return (
+    <div
+      style={{
+        top: `${currentPosition?.top}px`,
+        left: `${currentPosition?.left}px,`,
+        position: 'absolute',
+        padding: '1em',
+        backgroundColor: '#fff',
+        border: '1px solid #ccc',
+      }}
+      ref={wrapperRef}
+    >
+      {steps[currentStep].content}
+      {showPrev && <Button onClick={handlePrev}>Prev</Button>}
+      {showNext && <Button onClick={handleNext}>Next</Button>}
+    </div>
+  );
+}
 
-    return (
-        <>
-            {children[currentStep]}
-
-            {showPrev && <Button onClick={handlePrev}>Prev</Button>}
-            {showNext && <Button onClick={handleNext}>Next</Button>}
-        </>
-    );
+function updatePosition(step: InContextLearningStep, wrapperHeight: number) {
+  const {className} = step;
+  const offsetHeight = wrapperHeight / 2;
+  const domElement = document.querySelector(className);
+  if (!domElement) {
+    return {top: 0 + offsetHeight, left: 0};
+  }
+  const rect = domElement.getBoundingClientRect();
+  return {top: rect.top + offsetHeight, left: rect.left};
 }
 
 InContextLearning.Step = Step;
