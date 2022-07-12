e10a2cb13fb713ac81715a923b231fa8e63c713f
Chaz Dean <59836805+chazdean@users.noreply.github.com>
2022-07-12 11:20:57 -0400

    Update polaris-react/src/components/Heading/Heading.tsx
    Co-authored-by: Sara Hill <sara.hill@shopify.com>

--- polaris-react/src/components/Heading/Heading.tsx
+++ polaris-react/src/components/Heading/Heading.tsx
@@ -19,7 +19,7 @@ export interface HeadingProps {
 /**
  * @deprecated The Heading component will be removed in the next
  * major version. Use the Text componant instead. See the
- * Polaris componant guide on how to use Text.
+ * Polaris component guide on how to use Text.
  *
  * https://polaris.shopify.com/components/text
  */
